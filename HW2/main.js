9165adae83158270999a5173cfa9595e3baeb12c
ABergman7 <austin.bergman7@gmail.com>
2018-10-01 12:22:50 -0700

    Fixed HW2 branch (not a fast-forward)

---
+++ HW2/main.js
@@ -0,0 +1,30 @@
+function addNewCard(){
+    var word = document.getElementById('wordInput').value;
+    var define = document.getElementById('defInput').value;
+    
+    
+    
+    if(word.length == 0){
+        alert("Can't submit a blank card");
+        return;
+    }
+    else if (define.length == 0){
+        alert("You must submit a definition for your word");
+        return;
+    }
+    else
+        /*Make a flash card*/
+        $('col-md-6').append("<div class='card'><div class ='front'><span>"+word+"</span></div></div>")
+        $('col-md-6').append("<div class='card'><div class ='back'><span>"+define+"</span></div></div>")
+
+}
+
+
+function main(){
+    
+    $('.submit-btn').on('click', function(){ addNewCard(); $('#add')[0].reset();})
+                        
+    
+}
+
+$(document).ready(main);
