0c2bb047c5f0829a6338067cf3d515f71d167712
ABergman7 <austin.bergman7@gmail.com>
2018-10-03 17:49:56 -0700

    Site is up and running not designed well though

--- HW2/main.js
+++ HW2/main.js
@@ -1,20 +1,10 @@
-function main(){
-    
-    $('.submit-btn').on('click', addNewCard());
-                        
-    
-}
-
 
 function addNewCard() {
-    
-    console.log("addNewCard");
+  
     var word = document.getElementById('wordInput').value;
     var define = document.getElementById('defInput').value;
     
-    
-    
-    /*if(word.length == 0 ){
+    if(word.length == 0 ){
         alert("Can't submit a blank card");
         return;
     }
@@ -22,12 +12,23 @@ function addNewCard() {
         alert("You must submit a definition for your word");
         return;
     }
-    else
-    */
+    else {
+
         /*Make a flash card*/
-        $('.card-inner').append("<div class ='card-front'><span>"+word+"</span></div></div>")
-        $('.card-inner').append("<div class ='card-back'><span>"+define+"</span></div></div>")
-
+        $('.card-inner').append("<div class='card-front'><h1>"+word+"</h1></div><div class='card-back'><h2>"+define+"</h2></div></div>");
+        $('.deck').append("<dl><dt class ='dtTitle'>"+word+"</dt></dl>");
+        
+        
+    
+        
+        
+        
+        
+        
+    }
+    
+    
+    console.log(word);
+    
+    
 }
-
-$(document).ready(main);
