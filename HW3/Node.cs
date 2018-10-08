75aacd2b16f3b56dd17e5ade1bc5488a6baedaa6
ABergman7 <austin.bergman7@gmail.com>
2018-10-08 12:42:45 -0700

    Added ignore file

---
+++ HW3/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Node<T> 
+    {
+        public T data;
+        public Node<T> next;
+
+        public Node(T data, Node<T> next)
+        {
+            this.data = data;
+            this.next = next;
+        }
+    }
